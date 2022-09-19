277a9817c2d710b3892b0013ca7e3787ed2bfbb2
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-09-19 11:07:17 +0530

    Added: Vaccination Service Application

---
+++ VaccinationCenter/src/main/java/com/bridgelabz/vaccinationcenter/model/RequiredResponse.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.vaccinationcenter.model;
+
+import com.bridgelabz.vaccinationcenter.entity.VaccinationCenter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequiredResponse {
+
+    private VaccinationCenter center;
+    private List<Citizen> citizens;
+
+}
